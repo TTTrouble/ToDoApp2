740176e7ed8bfd1f9b68917f17e4108f49511523
Vijay <mannevijay@gmail.com>
2019-02-21 06:12:52 +0000

    add PATCH /todos/:d

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var ObjectID = require('mongodb').ObjectID;
+var _ = require('lodash');
+
 
 var mongoose = require('./db/mongoose').mongoose;
 var user = require('./models/user').user;
@@ -62,6 +64,32 @@ app.delete('/todos/:id', (req, res) => {
         }
         res.status(200).send(todo2);
     }).catch(e => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    //reason why he loaded lodash. Pick takes an object and array of properties you want to pull off to make user editable
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch(e=>res.status(400).send());
 });
 
 
